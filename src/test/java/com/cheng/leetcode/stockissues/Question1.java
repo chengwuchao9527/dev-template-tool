7f175b2181c8c49f9fe02737f9da9ccad4a889a7
chengwuchao@yzsci.cn <chougua9527@163.com>
2022-07-08 01:33:58 +0800

    feat:算法题

--- src/test/java/com/cheng/leetcode/stockissues/Question1.java
+++ src/test/java/com/cheng/leetcode/stockissues/Question1.java
@@ -3,6 +3,7 @@ package com.cheng.leetcode.stockissues;
 import org.junit.jupiter.api.Test;
 
 /**
+ * 分享：
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
  * <p>
  * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
@@ -48,6 +49,9 @@ public class Question1 {
         // 一次遍历
         int maximumProfit2 = getMaximumProfit2(prices);
         System.out.println(maximumProfit2);
+        //动态规划
+        int maximumProfit3 = getMaximumProfit3(prices);
+        System.out.println(maximumProfit3);
 
 
     }
@@ -90,5 +94,57 @@ public class Question1 {
         return max;
     }
 
+    public int getMaximumProfit3(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+
+        // dp[i][0] 下标为 i 这天结束的时候，不持股，手上拥有的现金数
+        // dp[i][1] 下标为 i 这天结束的时候，持股，手上拥有的现金数
+
+        // 初始化：不持股显然为 0，持股就需要减去第 1 天（下标为 0）的股价
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        // 从第 2 天开始遍历
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+
+    /**
+     * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+     *
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+     *
+     * 返回 你能获得的 最大 利润 。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：prices = [7,1,5,3,6,4]
+     * 输出：7
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+     *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
+     *      总利润为 4 + 3 = 7 。
+     * 示例 2：
+     *
+     * 输入：prices = [1,2,3,4,5]
+     * 输出：4
+     * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+     *      总利润为 4 。
+     * 示例 3：
+     *
+     * 输入：prices = [7,6,4,3,1]
+     * 输出：0
+     * 解释：在这种情况下, 交易无法获得正利润，所以不参与交易可以获得最大利润，最大利润为 0 。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
 
 }
